daffa408f1680015799f6eefacdf2d91e32d1ddb
Johannes Schacht <j.schacht@me.com>
2012-08-05 21:57:44 -0400

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapesfm::Application.config.secret_token = 'b6df6f5e080fe1248e02ffa7d9937efde8a2cdb90b6f5d378e85c98eff259d4b90423fe5a332ada4a1a899360f8be7e666d00de62abe9bde3e078b5abde21d75'
